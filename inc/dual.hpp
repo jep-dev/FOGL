2f6af48776e838d729284e020afce12641f03741
John Petersen <sobeita@gmail.com>
2016-01-27 18:11:01 -0500

    Separated declarations/comments from implementation.

--- inc/dual.hpp
+++ inc/dual.hpp
@@ -10,134 +10,206 @@ namespace Model {
 	template<typename R = float>
 	struct dual {
 		quat<R> u, v;
-		dual<R> operator=(dual<R> const& rhs) {
-			u = rhs.u; v = rhs.v;
-			return *this;
-		}
-		bool operator==(dual<R> const& rhs) const {
-			return u == rhs.u && v == rhs.v;
-		}
 
-		dual<R> operator+(dual<R> const& rhs) {
-			return {u+rhs.u, v+rhs.v};
-		}
-		dual<R> operator+=(dual<R> const& rhs) {
-			u += rhs.u; v += rhs.v;
-			return *this;
-		}
+		/* Unary operators */
+
+		/** Additive inverse */
+		dual<R> operator-(void) const;
+		/** Multiplicative inverse */
+		dual<R> operator!(void) const;
+		/** Distributes conjugation to members */
+		dual<R> operator~(void) const;
+		/** Cast operator, as Euclidean norm */
+		operator R(void) const;
+		/** Squared Euclidean norm */
+		R operator()(void) const;
+
+
+		/* Constant binary methods */
+
+		/** Distributes equality test */
+		bool operator==(dual<R> const& rhs) const;
+		/** Apply (lhs * rhs * ~lhs) */
+		dual<R> operator()(quat<R> const& rhs) const;
+		/** Apply (lhs * rhs * ~lhs) */
+		dual<R> operator()(dual<R> const& rhs) const;
+		dual<R> operator+(dual<R> const& rhs) const;
+		dual<R> operator-(quat<R> const& rhs) const;
+		dual<R> operator-(dual<R> const& rhs) const;
+		dual<R> operator*(R const& rhs) const;
+		dual<R> operator*(quat<R> const& rhs) const;
+		dual<R> operator*(dual<R> const& rhs) const;
+		dual<R> operator/(R const& rhs) const;
+		dual<R> operator/(quat<R> const& rhs) const;
+		dual<R> operator/(dual<R> const& rhs) const;
+
+		/* Binary assignment methods */
 		
-		dual<R> operator-(void) const {
-			return {-u, -v};
-		}
+		dual<R> operator=(dual<R> const& rhs);
+		dual<R> operator+=(dual<R> const& rhs);
+		dual<R> operator-=(quat<R> const& rhs);
+		dual<R> operator-=(dual<R> const& rhs);
+		dual<R> operator*=(R const& rhs);
+		dual<R> operator*=(quat<R> const& rhs);
+		dual<R> operator*=(dual<R> const& rhs);
+		dual<R> operator/=(R const& rhs);
+		dual<R> operator/=(quat<R> const& rhs);
+		dual<R> operator/=(dual<R> const& rhs);
 
-		dual<R> operator-(quat<R> const& rhs) const {
-			return {u-rhs, v};
-		}
-		dual<R> operator-=(quat<R> const& rhs) {
-			u -= rhs;
-			return *this;
-		}
-
-		dual<R> operator-(dual<R> const& rhs) const {
-			return {u-rhs.u, v-rhs.v};
-		}
-		dual<R> operator-=(dual<R> const& rhs) {
-			u -= rhs.u; v -= rhs.v;
-			return *this;
-		}
-
-		dual<R> operator*(const R rhs) const {
-			return {u*rhs, v*rhs};
-		}
-		dual<R> operator*=(const R rhs) {
-			u *= rhs; v *= rhs;
-			return *this;
-		}
-
-		dual<R> operator*(quat<R> const& rhs) const {
-			return {u*rhs, v*rhs};
-		}
-		dual<R> operator*=(quat<R> const& rhs) {
-			return *this = *this * rhs;
-		}
-		dual<R> operator*(dual<R> const& rhs) const {
-			auto const& p = rhs.u, q = rhs.v;
-			R auw = u.w, aux = u.x, auy = u.y, auz = u.z,
-			  avw = v.w, avx = v.x, avy = v.y, avz = v.z,
-			  buw = p.w, bux = p.x, buy = p.y, buz = p.z,
-			  bvw = q.w, bvx = q.x, bvy = q.y, bvz = q.z;
-			return {{
-				auw*buw - aux*bux - auy*buy - auz*buz,
-				auw*bux + aux*buw + auy*buz - auz*buy,
-				auw*buy + auy*buw + auz*buy - auy*buz,
-				auw*buz + auz*buw + aux*buy - auy*bux
-			}, {
-				avw*buw + avx*bux + avy*buy + avz*buz
-				+ auw*bvw - aux*bvx - auy*bvy - auz*bvz,
-				auw*bvx + aux*bvw - auy*bvz + auz*bvy
-				- avw*bux + avx*buw - avy*buz + avz*buy,
-				auw*bvy + aux*bvz + auy*bvw - auz*bvx
-				- avw*buy + avx*buz + avy*buw - avz*bux,
-				auw*bvz - aux*bvy + auy*bvx + auz*bvw
-				- avw*buz - avx*buy + avy*bux + avz*buw
-			}};
-		}
-		dual<R> operator*=(dual<R> const& rhs) {
-			return *this = *this * rhs;
-		}
-		
-		operator R(void) const {
-			return sqrt((*this)());
-		}
-		R operator()(void) const {
-			return u()+v();
-		}
-		dual<R> operator()(quat<R> const& rhs) const {
-			return *this * rhs * ~this;
-		}
-		dual<R> operator()(dual<R> const& rhs) const {
-			return *this * rhs * ~*this;
-		}
-
-
-		dual<R> operator~(void) const {
-			return {~u,{-v.w,v.x,v.y,v.z}};
-		}
-		dual<R> operator!(void) const {
-			dual<R> conj = ~*this;
-			return conj/conj();
-		}
-
-		dual<R> operator/(const R rhs) const {
-			return {u/rhs, v/rhs};
-		}
-		dual<R> operator/=(const R rhs) {
-			u /= rhs; v /= rhs;
-			return *this;
-		}
-		dual<R> operator/(quat<R> const& rhs) const {
-			auto inv = !rhs;
-			return {u*inv, v*inv};
-		}
-		dual<R> operator/=(quat<R> const& rhs) {
-			auto inv = !rhs;
-			u *= inv; v *= inv;
-			return *this;
-		}
-		dual<R> operator/(dual<R> const& rhs) const {
-			return *this * !rhs;
-		}
-		dual<R> operator/=(dual<R> const& rhs) {
-			return *this = *this * !rhs;
-		}
-
-		dual(quat<R> const& re, 
-				quat<R> const& du = R(0)):
-			u(re), v(du) {}
-		dual(R uw = R(0), R ux = R(0), R uy = R(0), R uz = R(0),
-			R vw = R(0), R vx = R(0), R vy = R(0), R vz = R(0)):
-			u(uw, ux, uy, uz), v(vw, vx, vy, vz) {}
+		dual(quat<R> const& re, quat<R> const& du = R(0));
+		dual(R uw = R(0), R ux = R(0), 
+				R uy = R(0), R uz = R(0), 
+				R vw = R(0), R vx = R(0), 
+				R vy = R(0), R vz = R(0));
 	};
+	
+	template<typename R>
+	dual<R> dual<R>::operator-(void) const {
+		return {-u, -v};
+	}
+	template<typename R>
+	dual<R> dual<R>::operator!(void) const {
+		dual<R> conj = ~*this;
+		return conj/conj();
+	}
+	template<typename R>
+	dual<R> dual<R>::operator~(void) const {
+		return {~u,{-v.w,v.x,v.y,v.z}};
+	}
+	template<typename R>
+	dual<R>::operator R(void) const {
+		return sqrt((*this)());
+	}
+	template<typename R>
+	R dual<R>::operator()(void) const {
+		return u()+v();
+	}
+
+	template<typename R>
+	bool dual<R>::operator==(dual<R> const& rhs) const {
+		return u == rhs.u && v == rhs.v;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator+(dual<R> const& rhs) const {
+		return {u+rhs.u, v+rhs.v};
+	}
+	template<typename R>
+	dual<R> dual<R>::operator-(quat<R> const& rhs) const {
+		return {u-rhs, v};
+	}
+	template<typename R>
+	dual<R> dual<R>::operator-(dual<R> const& rhs) const {
+		return {u-rhs.u, v-rhs.v};
+	}
+	template<typename R>
+	dual<R> dual<R>::operator*(R const& rhs) const {
+		return {u*rhs, v*rhs};
+	}
+	template<typename R>
+	dual<R> dual<R>::operator*(quat<R> const& rhs) const {
+		return {u*rhs, v*rhs};
+	}
+	template<typename R>
+	dual<R> dual<R>::operator*(dual<R> const& rhs) const {
+		auto const& p = rhs.u, q = rhs.v;
+		R auw = u.w, aux = u.x, auy = u.y, auz = u.z,
+		  avw = v.w, avx = v.x, avy = v.y, avz = v.z,
+		  buw = p.w, bux = p.x, buy = p.y, buz = p.z,
+		  bvw = q.w, bvx = q.x, bvy = q.y, bvz = q.z;
+		return {{
+			auw*buw - aux*bux - auy*buy - auz*buz,
+			auw*bux + aux*buw + auy*buz - auz*buy,
+			auw*buy + auy*buw + auz*buy - auy*buz,
+			auw*buz + auz*buw + aux*buy - auy*bux
+		}, {
+			avw*buw + avx*bux + avy*buy + avz*buz
+			+ auw*bvw - aux*bvx - auy*bvy - auz*bvz,
+			auw*bvx + aux*bvw - auy*bvz + auz*bvy
+			- avw*bux + avx*buw - avy*buz + avz*buy,
+			auw*bvy + aux*bvz + auy*bvw - auz*bvx
+			- avw*buy + avx*buz + avy*buw - avz*bux,
+			auw*bvz - aux*bvy + auy*bvx + auz*bvw
+			- avw*buz - avx*buy + avy*bux + avz*buw
+		}};
+	}
+	template<typename R>
+	dual<R> dual<R>::operator()(quat<R> const& rhs) const {
+		return *this * rhs * ~*this;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator()(dual<R> const& rhs) const {
+		return *this * rhs * ~*this;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator/(R const& rhs) const {
+		return {u/rhs, v/rhs};
+	}
+	template<typename R>
+	dual<R> dual<R>::operator/(quat<R> const& rhs) const {
+		auto inv = !rhs;
+		return {u*inv, v*inv};
+	}
+	template<typename R>
+	dual<R> dual<R>::operator/(dual<R> const& rhs) const {
+		return *this * !rhs;
+	}
+
+	template<typename R>
+	dual<R> dual<R>::operator=(dual<R> const& rhs) {
+		u = rhs.u; v = rhs.v;
+		return *this;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator+=(dual<R> const& rhs) {
+		u += rhs.u; v += rhs.v;
+		return *this;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator-=(quat<R> const& rhs) {
+		u -= rhs;
+		return *this;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator-=(dual<R> const& rhs) {
+		u -= rhs.u; v -= rhs.v;
+		return *this;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator*=(R const& rhs) {
+		u *= rhs; v *= rhs;
+		return *this;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator*=(quat<R> const& rhs) {
+		return *this = *this * rhs;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator*=(dual<R> const& rhs) {
+		return *this = *this * rhs;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator/=(R const& rhs) {
+		u /= rhs; v /= rhs;
+		return *this;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator/=(quat<R> const& rhs) {
+		auto inv = !rhs;
+		u *= inv; v *= inv;
+		return *this;
+	}
+	template<typename R>
+	dual<R> dual<R>::operator/=(dual<R> const& rhs) {
+		return *this = *this * !rhs;
+	}
+
+	template<typename R>
+	dual<R>::dual(quat<R> const& re, quat<R> const& du):
+		u(re), v(du) {}
+	template<typename R>
+	dual<R>::dual(R uw, R ux, R uy, R uz,
+			R vw, R vx, R vy, R vz):
+		u(uw, ux, uy, uz), v(vw, vx, vy, vz) {}
 }
 
 #endif
--- inc/quat.hpp
+++ inc/quat.hpp
@@ -8,100 +8,158 @@ namespace Model {
 	template<typename R = float>
 	struct quat {
 		R w, x, y, z;
-		quat<R> operator=(quat<R> const& rhs) {
-			w = rhs.w; x = rhs.x; y = rhs.y; z = rhs.z;
-			return *this;
-		}
-		bool operator==(quat<R> const& rhs) const {
-			return w == rhs.w && x == rhs.x 
-				&& y == rhs.y && z == rhs.z;
-		}
-	
-		quat<R> operator+(quat<R> const& rhs) {
-			return {w+rhs.w, x+rhs.x, y+rhs.y, z+rhs.z};
-		}
-		quat<R> operator+=(quat<R> const& rhs) const {
-			w += rhs.w; x += rhs.x; y += rhs.y; z += rhs.z;
-			return *this;
-		}
-
-		quat<R> operator-(void) const {
-			return {-w,-x,-y,-z};
-		}
-
-		quat<R> operator-(quat<R> const& rhs) const {
-			return {w-rhs.w, x-rhs.x, y-rhs.y, z-rhs.z};
-		}
-		quat<R> operator-=(quat<R> const& rhs) {
-			w -= rhs.w; x -= rhs.x; y -= rhs.y; z -= rhs.z;
-			return *this;
-		}
-
-		quat<R> operator*(quat<R> const& rhs) const {
-			R rw = rhs.w, rx = rhs.x, 
-			  ry = rhs.y, rz = rhs.z;
-			return {
-				w*rw - x*rx - y*ry - z*rz,
-				w*rx + x*rw + y*rz - z*ry,
-				w*ry + y*rw + rz*x - z*rx,
-				w*rz + z*rw + x*ry - y*rx};
-		}
-
-		quat<R> operator*=(quat<R> const& rhs) {
-			return *this = *this * rhs;
-		}
-		quat<R> operator*(const R rhs) const {
-			return {rhs*w, rhs*x, rhs*y, rhs*z};
-		}
-		quat<R> operator*=(const R rhs) {
-			w *= rhs; x *= rhs;
-			y *= rhs; z *= rhs;
-			return *this;
-		}
-
-		quat<R> operator~(void) const {
-			return {w,-x,-y,-z};
-		}
 		
-		operator R(void) const {
-			return sqrt((*this)());
-		}
-		R operator()(void) const {
-			return w*w + x*x + y*y + z*z;
-		}
-		quat<R> operator()(quat<R> const& rhs) const {
-			return *this * rhs * ~*this;
-		}
-		dual<R> operator()(dual<R> const& rhs) const {
-			return *this * rhs * ~*this;
-		}
+		/* Unary operators */
 
-		quat<R> operator!(void) const {
-			return ~(*this)/((*this)());
-		}
+		/** Multiplicative inverse */
+		quat<R> operator!(void) const;
+		/** Additive inverse */
+		quat<R> operator-(void) const;
+		/** Conjugate */
+		quat<R> operator~(void) const;
+		/** Cast operator, as Euclidean norm */
+		operator R(void) const;
+		/** Squared Euclidean norm */
+		R operator()(void) const;
+		/** Simple promotion */
+		operator dual<R>(void) const;
 
-		quat<R> operator/(quat<R> const& rhs) const {
-			return *this * !rhs;
-		}
-		quat<R> operator/=(quat<R> const& rhs) {
-			return *this = *this / rhs;
-		}
+		/* Constant binary methods (where lhs = *this) */
 
-		quat<R> operator/(const R rhs) const {
-			return {w/rhs, x/rhs, y/rhs, z/rhs};
-		}
+		/** Distributes equality test */
+		bool operator==(quat<R> const& rhs) const;
+		/** Apply (lhs * rhs * ~lhs) */
+		quat<R> operator()(quat<R> const& rhs) const;
+		/** Apply (lhs * rhs * ~lhs) */
+		dual<R> operator()(dual<R> const& rhs) const;
+		quat<R> operator+(quat<R> const& rhs) const;
+		quat<R> operator-(quat<R> const& rhs) const;
+		quat<R> operator*(R const& rhs) const;
+		quat<R> operator*(quat<R> const& rhs) const;
+		quat<R> operator/(R const& rhs) const;
+		quat<R> operator/(quat<R> const& rhs) const;
 
-		quat<R> operator/=(const R rhs) {
-			w /= rhs; x /= rhs;
-		}
+		/* Binary operators */
 
-		operator dual<R>(void) const {
-			return {*this, 0};
-		}
-		quat<R>(R w = R(0), R x = R(0), 
-				R y = R(0), R z = R(0)):
-			w(w), x(x), y(y), z(z) {}
+		quat<R> operator=(quat<R> const& rhs);
+		quat<R> operator+=(quat<R> const& rhs);
+		quat<R> operator-=(quat<R> const& rhs);
+		quat<R> operator*=(R const& rhs);
+		quat<R> operator*=(quat<R> const& rhs);
+		quat<R> operator/=(R const& rhs);
+		quat<R> operator/=(quat<R> const& rhs);
+
+		quat(R w = R(0), R x = R(0), 
+				R y = R(0), R z = R(0));
 	};
+
+	template<typename R>
+	quat<R> quat<R>::operator-(void) const {
+		return {-w,-x,-y,-z};
+	}
+	template<typename R>
+	quat<R> quat<R>::operator!(void) const {
+		return ~(*this)/((*this)());
+	}
+	template<typename R>
+	quat<R> quat<R>::operator~(void) const {
+		return {w,-x,-y,-z};
+	}
+	template<typename R>
+	quat<R>::operator R(void) const {
+		return sqrt((*this)());
+	}
+	template<typename R>
+	R quat<R>::operator()(void) const {
+		return w*w + x*x + y*y + z*z;
+	}
+	template<typename R>
+	quat<R>::operator dual<R>(void) const {
+		return {*this, 0};
+	}
+
+	template<typename R>
+	bool quat<R>::operator==(quat<R> const& rhs) const {
+		return w == rhs.w && x == rhs.x 
+			&& y == rhs.y && z == rhs.z;
+	}
+	template<typename R>
+	quat<R> quat<R>::operator()(quat<R> const& rhs) const {
+		return *this * rhs * ~*this;
+	}
+	template<typename R>
+	dual<R> quat<R>::operator()(dual<R> const& rhs) const {
+		return *this * rhs * ~*this;
+	}
+	template<typename R>
+	quat<R> quat<R>::operator+(quat<R> const& rhs) const {
+		return {w+rhs.w, x+rhs.x, y+rhs.y, z+rhs.z};
+	}
+	template<typename R>
+	quat<R> quat<R>::operator-(quat<R> const& rhs) const {
+		return {w-rhs.w, x-rhs.x, y-rhs.y, z-rhs.z};
+	}
+	template<typename R>
+	quat<R> quat<R>::operator*(R const& rhs) const {
+		return {rhs*w, rhs*x, rhs*y, rhs*z};
+	}
+	template<typename R>
+	quat<R> quat<R>::operator*(quat<R> const& rhs) const {
+		R rw = rhs.w, rx = rhs.x, 
+		  ry = rhs.y, rz = rhs.z;
+		return {
+			w*rw - x*rx - y*ry - z*rz,
+			w*rx + x*rw + y*rz - z*ry,
+			w*ry + y*rw + rz*x - z*rx,
+			w*rz + z*rw + x*ry - y*rx};
+	}
+	template<typename R>
+	quat<R> quat<R>::operator/(R const& rhs) const {
+		return {w/rhs, x/rhs, y/rhs, z/rhs};
+	}
+	template<typename R>
+	quat<R> quat<R>::operator/(quat<R> const& rhs) const {
+		return *this * !rhs;
+	}
+
+	template<typename R>
+	quat<R> quat<R>::operator=(quat<R> const& rhs) {
+		w = rhs.w; x = rhs.x; y = rhs.y; z = rhs.z;
+		return *this;
+	}
+	template<typename R>
+	quat<R> quat<R>::operator+=(quat<R> const& rhs) {
+		w += rhs.w; x += rhs.x; y += rhs.y; z += rhs.z;
+		return *this;
+	}
+	template<typename R>
+	quat<R> quat<R>::operator-=(quat<R> const& rhs) {
+		w -= rhs.w; x -= rhs.x; y -= rhs.y; z -= rhs.z;
+		return *this;
+	}
+	template<typename R>
+	quat<R> quat<R>::operator*=(R const& rhs) {
+		w *= rhs; x *= rhs;
+		y *= rhs; z *= rhs;
+		return *this;
+	}
+	template<typename R>
+	quat<R> quat<R>::operator*=(quat<R> const& rhs) {
+		return *this = *this * rhs;
+	}
+	template<typename R>
+	quat<R> quat<R>::operator/=(R const& rhs) {
+		w /= rhs; x /= rhs;
+		return *this;
+	}
+	template<typename R>
+	quat<R> quat<R>::operator/=(quat<R> const& rhs) {
+		return *this = *this / rhs;
+	}
+
+	template<typename R>
+	quat<R>::quat(R w, R x, R y, R z):
+		w(w), x(x), y(y), z(z) {}
 }
 
 #endif
