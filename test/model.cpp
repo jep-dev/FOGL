7ed574403e17991bd7d72ffaf498ee50499a87cc
John Petersen <sobeita@gmail.com>
2016-02-01 02:18:06 -0500

    Added separate test case for quaternions; globals will be replaced with fixtures.

--- test/model.cpp
+++ test/model.cpp
@@ -3,22 +3,21 @@
 
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE Model
-#include <boost/test/included/unit_test_framework.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 #include "../inc/model.hpp"
 using namespace Model;
 
-BOOST_AUTO_TEST_CASE(dual_quaternions) {
-	dual<float> A,B,
-		one(1), 
-		i(0,1), 
-		j(0,0,1),
-		k(0,0,0,1);
+const quat<float> R0(0), R1(1), 
+	  i(0,1), j(0,0,1), k(0,0,0,1);
+const dual<float> E0(R0, 0), E1(R0, 1), 
+	  Ei = E1*i, Ej = E1*j, Ek = E1*k;
 
-	BOOST_TEST_CONTEXT("A = " << (A = i) << ";   B = " << (B = one+k)) {
-		BOOST_CHECK(float(A) == 1);
-		BOOST_CHECK(A*j == k);
-		BOOST_CHECK(j*A ==-k);
-		BOOST_CHECK(float(B) == float(sqrt(2)));
-	}
+BOOST_AUTO_TEST_CASE(quaternions) {
+	BOOST_CHECK_EQUAL(i*j, k);
+	BOOST_CHECK_EQUAL(j*i,-k);
+}
+
+BOOST_AUTO_TEST_CASE(dual_quaternions) {
+	BOOST_CHECK_EQUAL(E1*E1, E0);
 }
