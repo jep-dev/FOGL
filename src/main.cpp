154033fcb33e3a69e3c490da7dddf17a7210e19c
John Petersen <sobeita@gmail.com>
2016-02-01 02:14:00 -0500

    Added proper threading; async may only defer

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,41 @@
 #include "../inc/model.hpp"
 #include "../inc/view.hpp"
 
-#include <future>
-#include <functional>
-
+#include <X11/Xlib.h>
+#include <thread>
 #include <iostream>
 
+#include <SFML/System/Time.hpp>
+#include <SFML/System/Sleep.hpp>
+
 int main(int argc, const char **argv) {
-	View::view v(512, 512, "View");
-	std::async(std::launch::async, [&v]{
-		v.run([](void){});
-	});
+	XInitThreads();
+	View::view display(512, 512, "View");
+	
+	display.win.setActive(false);
+	auto runFrame = []{
+		// One view frame
+	};
+	auto runModel = [&display]{
+		// One model frame
+		display.win.setActive(false);
+		static int t = 0;
+		while(!display.done) {
+			std::cout << "Model: t = " << t++ << std::endl;
+			sf::sleep(sf::milliseconds(1000));
+		}
+	};
+	auto runView = [&display, &runFrame]{
+		display.run(runFrame);
+	};
+
+	std::thread viewThread(runView), 
+		modelThread(runModel);
+
+	viewThread.join();
+	modelThread.join();
+
+	//Model::test();
 
 	return 0;
 }
