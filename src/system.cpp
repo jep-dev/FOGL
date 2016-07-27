3ad91b350b92060e11a790150e3f42d44a7ce00f
John <sobeita@gmail.com>
2016-07-27 12:29:53 -0400

    (Definitions/implementations from header)

--- src/system.cpp
+++ src/system.cpp
@@ -1,6 +1,63 @@
 #include "system.hpp"
+#include <fstream>
 
 namespace System {
+	bool uni_special(char ch) {
+		return (ch & 0xc0) == 0x80;
+	}
+	int uni_strlen(const char *word) {
+		int len = 0;
+		for(; *word; word++) {
+			if(!uni_special(*word)) {
+				len++;
+			}
+		}
+		return len;
+	}
+	int uni_strlen(const std::string &word) {
+		return uni_strlen(word.c_str());
+	}
+
+	int strlen(const char *word) {
+		int len = 0;
+		bool escape = false, bracket = false;
+		for(; *word; word++) {
+			char ch = *word;
+			if(!uni_special(ch)) {
+				if(bracket) {
+					if(ch == 'm') {
+						bracket = escape = false;
+					}
+				} else if(escape) {
+					if(ch == '[') {
+						bracket = true;
+					}
+				} else if(ch == '\e') {
+					escape = true;
+				} else {
+					len++;
+				}
+			}
+		}
+		return len;
+	}
+
+	std::string repeat(int w, char c = ' ') {
+		std::ostringstream oss;
+		oss << std::setw(w) << std::setfill(c);
+		return oss.str();
+	}
+	template<typename T>
+	std::string stringify(const T &t) {
+		std::ostringstream oss;
+		oss << t;
+		return oss.str();
+	}
+	template<typename T1, typename T2, typename... TN>
+	std::string stringify(const T1 &t1, const T2 &t2, const TN &... tn) {
+		return stringify(t1) + " " + stringify(t2, tn...);
+	}
+
 	bool readFile(const char *fname, std::string &lines) {
 		std::ifstream file(fname);
 		if(!file.is_open()) {
