818bed7e00c058ced1175010b8e0267ab5ec07a6
John <sobeita@gmail.com>
2016-07-27 23:47:41 -0400

    Added forward declarations and comments

--- include/model.hpp
+++ include/model.hpp
@@ -2,6 +2,18 @@
 #define MODEL_HPP
 
 namespace Model {
+	namespace Ply {
+		/** @brief A structure representing a PLY primitive data type */
+		struct Primitive;
+		/** @brief A structure representing instances of a single property */
+		struct Property;
+		/** @brief A vector/stack of raw bytes (needs replacement) */
+		struct Buffer;
+		/** @brief A grouping of properties */
+		struct Element;
+		/** @brief A grouping of declarations */
+		struct Header;
+	} // --> 
 }
 
 #endif
