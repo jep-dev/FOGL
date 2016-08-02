36af7bc601894e2da8c14598ec13f5f97f3312de
John <sobeita@gmail.com>
2016-08-02 13:54:19 -0400

    Changed cases to match common convention

--- src/math/affine.cpp
+++ src/math/affine.cpp
@@ -4,26 +4,26 @@
 
 namespace Math {
 	template<typename R> std::ostream&
-	operator<<(std::ostream &lhs, const Unit<R> &rhs) {
+	operator<<(std::ostream &lhs, const unit<R> &rhs) {
 		return lhs << "<" << rhs.x << ", " << rhs.y
 			<< ", " << rhs.z << ">";
 	}
 	template<typename R> std::ostream&
-	operator<<(std::ostream &lhs, const Ray<R> &rhs) {
+	operator<<(std::ostream &lhs, const ray<R> &rhs) {
 		return lhs << rhs.r << " * " << rhs.n;
 	}
 	template<typename R> std::ostream&
-	operator<<(std::ostream& lhs, const Rotor<R> &rhs) {
+	operator<<(std::ostream& lhs, const rotor<R> &rhs) {
 		return lhs << (rhs.theta/M_PI) 
 			<< "pi; " << rhs.n;
 	}
 	template<typename R> std::ostream&
-	operator<<(std::ostream& lhs, const Point<R> &rhs) {
+	operator<<(std::ostream& lhs, const point<R> &rhs) {
 		return lhs << "{" << rhs.x << ", " << rhs.y
 			<< ", " << rhs.z << "}";
 	}
 	template<typename R> std::ostream&
-	operator<<(std::ostream& lhs, const Pivot<R> &rhs) {
+	operator<<(std::ostream& lhs, const pivot<R> &rhs) {
 		return lhs << "Pivot: {" << rhs.rotation << "} "
 			"from center <" << rhs.translation << ">";
 	}
