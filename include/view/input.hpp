f48c37c4ee84f87dad1f8677af716aaa23867e09
John Petersen <sobeita@gmail.com>
2016-08-09 05:33:50 -0400

    (Minor tweaks)

--- include/view/input.hpp
+++ include/view/input.hpp
@@ -1,34 +1,34 @@
 #ifndef VIEW_INPUT_HPP
 #define VIEW_INPUT_HPP
 
-//#include "view.hpp"
+#include "view.hpp"
 #include <GLFW/glfw3.h>
 
 namespace View {
 	namespace Input {
 		/// Keyboard event callback type
-		typedef void (*ecb_kbd)(GLFWwindow*, int, int, int, int);
+		typedef void (*cb_kbd)(GLFWwindow*, int, int, int, int);
 
 		/// Character input event callback type
-		typedef void (*ecb_char)(GLFWwindow*, unsigned int);
+		typedef void (*cb_char)(GLFWwindow*, unsigned int);
 
 		/// Mouse button press event callback type
-		typedef void (*ecb_mpress)(GLFWwindow*, unsigned int);
+		typedef void (*cb_mpress)(GLFWwindow*, unsigned int);
 
 		/// Drag-and-drop event callback type
-		typedef void (*ecb_mdrop)(GLFWwindow*, int, const char**);
+		typedef void (*cb_mdrop)(GLFWwindow*, int, const char**);
 
 		/// Mouse movement event callback type
-		typedef void (*ecb_mmove)(GLFWwindow*, double, double);
+		typedef void (*cb_mmove)(GLFWwindow*, double, double);
 
 		/// Mouse crossing (enter/exit) callback type
-		typedef void (*ecb_mcross)(GLFWwindow*, int);
+		typedef void (*cb_mcross)(GLFWwindow*, int);
 
 		/// Scroll event callback type
-		typedef void (*ecb_mscroll)(GLFWwindow*, double, double);
+		typedef void (*cb_mscroll)(GLFWwindow*, double, double);
 
 		/// Joystick movement event callback type
-		typedef void (*ecb_jmove)(GLFWwindow*, int, int);
+		typedef void (*cb_jmove)(GLFWwindow*, int, int);
 	}
 }
 
