464d73c92a689c2b9728c9d03728ca90239fa75f
John Petersen <sobeita@gmail.com>
2016-08-09 00:23:05 -0400

    Replaced std threads with OpenMP directives

--- include/main.hpp
+++ include/main.hpp
@@ -1,6 +1,13 @@
 #ifndef MAIN_HPP
 #define MAIN_HPP
-/// @file
+
+
+typedef enum : unsigned char {
+	e_tid_view=0, ///< The view thread ID, necessarily 0 (master)
+	e_tid_model,  ///< The model thread ID
+	e_tid_total   ///< The total number of enumerated threads
+} e_tid; ///< The ID of a thread, where 0 is the master
+
 /**
  * Entry point of the program
  * @param argc Number of args (1+)
--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,60 @@
 #include "main.hpp"
 
-#include "util.hpp"
-#include "util/types.hpp"
-#include "util/functional.hpp"
-
+//#include "util.hpp"
 #include "system.hpp"
-#include "system/net.hpp"
-
-#include "math.hpp"
-#include "math/quat.hpp"
-#include "math/dual.hpp"
-#include "math/affine.hpp"
-
+//#include "math.hpp"
 #include "model.hpp"
-#include "model/ply.hpp"
-
 #include "view.hpp"
-#include "view/shade.hpp"
 
-#include <atomic>
 #include <chrono>
 #include <iostream>
-#include <iomanip>
-#include <ostream>
-#include <sstream>
-#include <string>
-#include <typeinfo>
 #include <thread>
 
 #include "omp.h"
 #include <boost/asio.hpp>
-#include <boost/asio/yield.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
 using namespace System;
 using PB = Printer_Base;
 
 int main(int argc, const char **argv) {
-	const short port = 5000;
-	const int workers = 3;
-
-	// OpenMP stub;
-	// TODO migrate from std thread over closures to OMP
-	/* int id;
- 	#pragma omp parallel num_threads(3) \
-	shared(port, workers) private(id)
-	{
-		//switch(omp_get_thread_num()) {}
-	}*/
-
 	std::atomic_bool alive(true);
 	auto delay = std::chrono::milliseconds(150);
-	
-	auto quit = [&alive]() {
-		alive = false;
-	};
-	auto updateView = []() {
-		return true;
-	};
 
-	// Update model based on state & delay
-	auto updateModel = [&alive, &delay]{
-		static int t = 0;
-		while(alive) {
-			std::cout << "Model update #" << t++ << std::endl;
-			std::this_thread::sleep_for(delay);
+	int tid;
+ 	#pragma omp parallel num_threads(e_tid_total) \
+	shared(alive, delay) private(tid)
+	{
+		switch(omp_get_thread_num()) {
+			case e_tid_view: {
+				auto quit = [&alive]() {
+					alive = false;
+				};
+				auto updateView = []() {
+					return true;
+				};
+				View::view display("share/shade.vert", "share/shade.frag");
+				if(display.valid) {
+					display.run(updateView, quit);
+				}
+			} break;
+			case e_tid_model: {
+				int t = 0;
+				auto updateModel = [&alive, &delay]{
+					static int t = 0;
+					while(alive) {
+						std::cout << "Model update #" << t++ << std::endl;
+						std::this_thread::sleep_for(delay);
+					}
+				};
+				
+				while(alive) {
+					std::cout << "Model update #" << t++ << std::endl;
+					std::this_thread::sleep_for(delay);
+				}
+			} break;
+			default: break;
 		}
-	};
-
-	View::view display("share/shade.vert", "share/shade.frag");
-	if(display.valid) {
-		std::thread modelThread(updateModel);
-		display.run(updateView, quit);
-		modelThread.join();
 	}
 
 	glfwTerminate();
