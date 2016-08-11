7cf84957f51626243b8ddb6239347ea7944f6547
John Petersen <sobeita@gmail.com>
2016-08-10 23:45:44 -0400

    Began development of obj parser

---
+++ include/model/obj.hpp
@@ -0,0 +1,72 @@
+#ifndef OBJ_HPP
+#define OBJ_HPP
+
+namespace Model {
+	/// An object 
+	struct obj_t {
+		/*! The enumeration and count of possible states */
+		typedef enum : uint8_t {
+			e_status_ok=0,   ///< Parsing status is OK
+			e_status_io,     ///< An IO exception has occurred
+			e_status_unknown,///< An unknown exception has occurred
+			e_status_total   ///< The total number of statuses
+		} e_obj_status;
+
+		/*! An enumeration of supported types */
+		typedef enum : uint8_t {
+			e_element_comment=0, ///< \ref element_t
+			e_element_face,      ///< \ref face_t
+			e_element_group,     ///< \ref group_t
+			e_element_line,      ///< \ref line_t
+			e_element_vertex,    ///< \ref vertex_t
+			e_element_total      ///< The total number of supported types
+		} e_obj_element;
+
+		static constexpr const char *prefixes[e_element_total]{
+			"#", "f", "g", "l", "v"
+		};
+
+		/// The abstract base of each obj element
+		struct element_t {
+		protected:
+			e_obj_element type;
+		};
+
+		/// A comment, used only for storage
+		struct comment_t : element_t {
+			const char *contents;
+		protected:
+			const e_obj_element type = e_element_comment;
+		};
+
+		/// A set of vertices with or without texture coordinates
+		struct face_t : element_t {
+			unsigned int *vertices, *coordinates;
+			bool tex_coords = false;
+		protected:
+			const e_obj_element type = e_element_face;
+		};
+
+		/// A collection of faces, lines, and vertices
+		struct group_t : element_t {
+		protected:
+			const e_obj_element type = e_element_group;
+		};
+
+		/// A pair of vertex indices 
+		struct line_t : element_t {
+		protected:
+			const e_obj_element type = e_element_line;
+		};
+
+		/// A single point, containing at least x, y, z coordinates
+		struct vertex_t : element_t {
+		protected:
+			const e_obj_element type = e_element_vertex;
+		};
+
+		static e_obj_status load(const char *fname, element_t** elements);
+	};
+}
+
+#endif
---
+++ src/model/obj.cpp
@@ -0,0 +1,55 @@
+#include "model/obj.hpp"
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <boost/tokenizer.hpp>
+
+namespace Model {
+	static e_obj_status load(const char *fname, element_t** elements) {
+		std::ifstream file;
+		file.open(fname, std::ios::in);
+		if(!file.is_open()) {
+			return e_status_io;
+		}
+		for(std::string line; std::getline(file, line);) {
+			boost::tokenizer<> tk(line);
+			bool flip = true;
+			e_obj_element element;
+			e_obj_status status = e_status_unknown;
+			for(auto it = std::begin(tk); it != std::end(tk); ++it) {
+				auto word = *it;
+				if(flip) {
+					flip = false;
+					for(e_obj_element oe = 0; oe < e_element_total; oe++) {
+						if(word == prefixes[oe]) {
+							element = oe;
+						}
+					}
+					switch(element) {
+					case e_element_comment: {
+						// TODO
+					} break;
+					case e_element_face: {
+						// TODO 
+					} break;
+					case e_element_group: {
+						// TODO
+					} break;
+					case e_element_line: {
+						// TODO
+					} break;
+					case e_element_vertex: {
+						// TODO
+					} break;
+					default: {
+						element = e_element_total;
+						status = e_obj_status;
+					} break;
+					}
+				}
+			}
+		}
+		file.close();
+	}
+}
