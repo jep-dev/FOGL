a41b0b73058960a6db87a1690d6ad49c7c79d51b
John Petersen <sobeita@gmail.com>
2016-08-16 16:51:10 -0400

    Removed redundant typedefs (each had a glfw*fun type defined already)

--- include/view/input.hpp
+++ include/view/input.hpp
@@ -9,34 +9,6 @@ namespace View {
 			e_cb_char=0, e_cb_jmove, e_cb_kbd, e_cb_mcross,
 			e_cb_mdrop, e_cb_mmove, e_cb_mpress, e_cb_mscroll
 		};
-
-		/// Generic callback type
-		typedef void (*cb_t)();
-
-		/// Keyboard event callback type
-		typedef void (*cb_kbd)(GLFWwindow*, int, int, int, int);
-
-		/// Character input event callback type
-		typedef void (*cb_char)(GLFWwindow*, unsigned int);
-
-		/// Mouse button press event callback type
-		typedef void (*cb_mpress)(GLFWwindow*, unsigned int);
-
-		/// Drag-and-drop event callback type
-		typedef void (*cb_mdrop)(GLFWwindow*, int, const char**);
-
-		/// Mouse movement event callback type
-		typedef void (*cb_mmove)(GLFWwindow*, double, double);
-
-		/// Mouse crossing (enter/exit) callback type
-		typedef void (*cb_mcross)(GLFWwindow*, int);
-
-		/// Scroll event callback type
-		typedef void (*cb_mscroll)(GLFWwindow*, double, double);
-
-		/// Joystick movement event callback type
-		typedef void (*cb_jmove)(GLFWwindow*, int, int);
-
 	}
 }
 
