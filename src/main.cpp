43d46936106bbbcd2e18d8bc4888c173cf9afe72
John Petersen <sobeita@gmail.com>
2016-08-16 10:14:13 -0400

    Removed math testing section

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,11 @@
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
-using namespace System;
-using PB = Printer_Base;
-
 int main(int argc, const char **argv) {
-	using namespace Math;
-	auto __1 = quat<float>{1,0,0,0},
-		 __2 = quat<float>{0,1,0,0},
-		 __3 = quat<float>{0,0,1,0},
-		 __4 = quat<float>{0,0,0,1};
-	auto ident = quat<quat<float>>{__1,__2,__3,__4};
-
 	glfwInit();
 	std::atomic_bool alive(true);
 	Control::control ctl(alive, "share/shade.vert", "share/shade.frag");
-	ctl.run(alive);
+	Util::task::run(alive, &ctl);
+	glfwTerminate();
 	return 0;
 }
