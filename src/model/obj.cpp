10034529d2a54572ed138326710032122f1677e2
John Petersen <sobeita@gmail.com>
2016-08-18 13:32:25 -0400

    Fixed return status and condensed a switch

--- src/model/obj.cpp
+++ src/model/obj.cpp
@@ -6,46 +6,41 @@
 #include <boost/tokenizer.hpp>
 
 namespace Model {
-	static e_obj_status load(const char *fname, element_t** elements) {
+	obj_t::e_obj_status obj_t::load(const char *fname, element_t** elements) {
 		std::ifstream file;
 		file.open(fname, std::ios::in);
 		if(!file.is_open()) {
 			return e_status_io;
 		}
+		e_obj_element element;
+		e_obj_status status = e_status_unknown;
 		for(std::string line; std::getline(file, line);) {
 			boost::tokenizer<> tk(line);
 			bool flip = true;
-			e_obj_element element;
-			e_obj_status status = e_status_unknown;
 			for(auto it = std::begin(tk); it != std::end(tk); ++it) {
 				auto word = *it;
 				if(flip) {
 					flip = false;
-					for(e_obj_element oe = 0; oe < e_element_total; oe++) {
+					for(uint8_t oe = 0; oe < e_element_total; oe++) {
 						if(word == prefixes[oe]) {
-							element = oe;
+							element = static_cast<e_obj_element>(oe);
 						}
 					}
-					// TODO Parsing
+					// TODO Parse obj types
 					switch(element) {
-					case e_element_comment: {
-					} break;
-					case e_element_face: {
-					} break;
-					case e_element_group: {
-					} break;
-					case e_element_line: {
-					} break;
-					case e_element_vertex: {
-					} break;
-					default: {
+					case e_element_comment: break;
+					case e_element_face: break;
+					case e_element_group: break;
+					case e_element_line: break;
+					case e_element_vertex: break;
+					default:
 						element = e_element_total;
-						status = e_obj_status;
-					} break;
+						status = e_status_unknown;
 					}
 				}
 			}
 		}
 		file.close();
+		return status;
 	}
 }
