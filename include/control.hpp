379ccc39cf295b28818b136e5c46e47160adb4ef
John Petersen <sobeita@gmail.com>
2016-08-25 17:23:44 -0400

    Changed the enumerated thread ID's to task ID's (superficial)

--- include/control.hpp
+++ include/control.hpp
@@ -12,10 +12,10 @@
 
 namespace Control {
 	typedef enum : unsigned char {
-		e_tid_view=0, ///< The view thread ID, necessarily 0 (master)
-		e_tid_model,  ///< The model thread ID
-		e_tid_total   ///< The total number of enumerated threads
-	} e_tid; ///< The ID of a thread, where 0 is the master
+		e_tid_view=0, ///< The view task ID, necessarily 0 (master)
+		e_tid_model,  ///< The model task ID
+		e_tid_total   ///< The total number of enumerated tasks
+	} e_tid; ///< The ID of a task
 
 	/// A data structure controlling both model and view
 	struct control : virtual Util::task {
