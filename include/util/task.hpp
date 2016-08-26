247e91f4b085504c0ebe2b019368f1e8d6d1a515
John Petersen <sobeita@gmail.com>
2016-08-26 16:06:54 -0400

    Added worker type

--- include/util/task.hpp
+++ include/util/task.hpp
@@ -11,6 +11,10 @@ namespace Util {
  		 * @param alive Shared status; false signals shutdown
  		 */
 		virtual void init(std::atomic_bool& alive) =0;
+		/** Polls for changes
+ 		 * @param alive Shared status; false signals shutdown
+ 		 */
+		virtual void poll(std::atomic_bool &alive) =0;
 		/** Runs a given task
  		 * @param alive Shared status; false signals shutdown
  		 */
@@ -22,6 +26,9 @@ namespace Util {
  		 * @param t The task to initialize */
 		static void init(std::atomic_bool &alive, task *t) {
 			t -> init(alive);
+		}
+		static void poll(std::atomic_bool &alive, task *t) {
+			t -> poll(alive);
 		}
 		/** Runs a given task
  		 * @param alive Shared status; false signals shutdown
@@ -39,12 +46,37 @@ namespace Util {
 				t -> init(alive);
 			}
 		}
+		void poll(std::atomic_bool& alive) {
+			for(auto t : tasks) {
+				t -> poll(alive);
+			}
+		}
 		void run(std::atomic_bool& alive) {
 			for(auto t : tasks) {
 				t -> run(alive);
 			}
 		}
 		virtual ~supertask(void) {}
+	};
+
+	struct worker : public virtual task {
+		std::function<void (std::atomic_bool &)>
+			m_init, m_poll, m_run;
+		//void (*m_run)(std::atomic_bool &);
+		void init(std::atomic_bool &alive) {
+			m_init(alive);
+		}
+		void poll(std::atomic_bool &alive) {
+			m_poll(alive);
+		}
+		void run(std::atomic_bool &alive) {
+			m_run(alive);
+		}
+		worker(std::function<void (std::atomic_bool &)> m_init,
+				std::function<void (std::atomic_bool &)> m_poll,
+				std::function<void (std::atomic_bool &)> m_run):
+			m_init(m_init), m_poll(m_poll), m_run(m_run) {}
+		virtual ~worker(void) {}
 	};
 }
 
