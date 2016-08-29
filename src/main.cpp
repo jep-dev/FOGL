fab262f92bfca582302c5257feb19cde08d1dd96
John Petersen <sobeita@gmail.com>
2016-08-29 12:12:25 -0400

    Added guards for control failure during construction, init and run

--- src/main.cpp
+++ src/main.cpp
@@ -13,15 +13,36 @@
 
 int main(int argc, const char **argv) {
 	using namespace Util;
-	std::atomic_bool alive(true);
 	if(glfwInit() == 0) {
 		std::cout << "Failed to initialize GLFW "
 			<< glfwGetVersionString() << std::endl;
 		return 1;
 	}
-	Control::control ctl(alive, OBJ_PATH, VERT_PATH, FRAG_PATH);
-	task::init(alive, &ctl);
-	task::run(alive, &ctl);
+
+	std::atomic_bool alive(true);
+	const char *obj_fname, *vert_fname, *frag_fname;
+
+	if(argc >= 2) obj_fname = argv[1];
+	else obj_fname = OBJ_PATH;
+	if(argc >= 3) vert_fname = argv[2];
+	else vert_fname = VERT_PATH;
+	if(argc >= 4) frag_fname = argv[3];
+	else frag_fname = FRAG_PATH;
+
+	Control::control ctl(alive, obj_fname, vert_fname, frag_fname);
+	if(alive) {
+		task::init(alive, &ctl);
+	} else {
+		std::cout << "Initialization of control failed." << std::endl;
+		return 1;
+	}
+	if(alive) {
+		task::run(alive, &ctl);
+	} else {
+		std::cout << "Control failed while running." << std::endl;
+	}
+
+
 	glfwTerminate();
 
 	return 0;
