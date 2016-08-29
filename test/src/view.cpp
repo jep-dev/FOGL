34ee64507f151f176d769a94ff97ce467fd5ed5b
John Petersen <sobeita@gmail.com>
2016-08-29 17:00:18 -0400

    Added more error checking but could not determine why GLFW can init but not terminate

--- test/src/view.cpp
+++ test/src/view.cpp
@@ -1,6 +1,7 @@
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
 #include "view.hpp"
 #include <iostream>
-#include <GLFW/glfw3.h>
 
 #ifndef GLFW_TRUE
 #define GLFW_TRUE 1
@@ -15,12 +16,21 @@ int main(int argc, const char **argv) {
 		std::cout << "Failed to initialize GLFW "
 			<< glfwGetVersionString() << std::endl;
 		return 1;
+	} else {
+		std::atomic_bool alive(true);
+		view v(alive, "share/shade.vert", "share/shade.frag");
+		if(alive) v.init(alive);
+		else {
+			std::cout << "Failed to construct view." << std::endl;
+			return 1;
+		}
+		if(alive) v.run(alive);
+		else {
+			std::cout << "Failed to run view." << std::endl;
+			return 1;
+		}
+		glfwTerminate();
 	}
-	std::atomic_bool alive(true);
-	view v(alive, "share/shade.vert", "share/shade.frag");
-	v.init(alive);
-	v.run(alive);
-	glfwTerminate();
 
 	// TODO Determine why GLFW can init but cannot terminate
 }
