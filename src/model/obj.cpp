e9ea3de4a189182ba484b8345e5b8928b14b4614
John Petersen <sobeita@gmail.com>
2016-08-31 22:01:25 -0400

    One step closer to completing wavefront obj parsing; vertices, norms, and faces can be extracted by range

--- src/model/obj.cpp
+++ src/model/obj.cpp
@@ -10,9 +10,28 @@
 #include <boost/lexical_cast.hpp>
 
 namespace Model {
-	e_el obj_t::parse_type(std::string word) {
+	e_el obj_t::parse_type(std::string line) {
+		std::istringstream iss(line);
+		std::string word;
+		iss >> word;
 		if(word == element_t<e_el_c>::prefix) return e_el_c;
-		if(word == element_t<e_el_f0>::prefix) return e_el_f0;
+		if(word == element_t<e_el_f0>::prefix) {
+			auto type = e_el_f0;
+			auto firstSlash = word.find("/"),
+			 	 lastSlash = word.rfind("/");
+			if(firstSlash != std::string::npos) {
+				if(firstSlash == lastSlash) {
+					type = e_el_f1;
+				} else if(firstSlash == lastSlash-1) {
+					type = e_el_f2;
+				} else {
+					type = e_el_f3;
+				}
+			} else {
+				type = e_el_f0;
+			}
+			return type;
+		}
 		if(word == element_t<e_el_g>::prefix) return e_el_g;
 		if(word == element_t<e_el_l>::prefix) return e_el_l;
 		if(word == element_t<e_el_m>::prefix) return e_el_m;
@@ -25,154 +44,68 @@ namespace Model {
 		return e_el_total;
 	}
 
-	obj_t::e_status obj_t::parse(std::string line, const char *delim) {
+	obj_t::e_status obj_t::parse(std::string line, e_el type) {
 		auto status = obj_t::e_status::e_ok;
 		boost::tokenizer<boost::char_separator<char>>
-			tk(line, boost::char_separator<char>(delim)),
+			tk(line, boost::char_separator<char>(" ")),
 			s_tk(line, boost::char_separator<char>(" /"));
-		int i = 0, j = 0;
-		auto prev = e_el_total;
 		for(auto it = std::begin(tk); it != std::end(tk); ++it) {
 			auto word = *it++;
-			auto type = parse_type(word);
-			auto has_strings = false, has_floats = false,
-				 has_ints = false, has_bools = false;
+			int n_bools = 0, n_floats = 0, n_ints = 0, n_strings = 0;
 			if(type == e_el_s) {
-				has_bools = true;
 				if(it == std::end(tk)) {
 					bools.emplace_back(true);
 				} else {
 					auto word = *it++;
 					bools.emplace_back(word == "1" || word == "on");
 				}
-				nBools.emplace_back(1);
-			} else {
-				nBools.emplace_back(0);
+				n_bools++;
 			}
-			if((mask_has_floats & (1<<type)) != 0) {
-				has_floats = true;
+			if(mask_has_floats & (1<<type)) {
 				float val;
-				int nValues = 0;
 				while(it != std::end(tk)) {
 					val = boost::lexical_cast<float>(*it++);
 					floats.push_back(val);
-					nValues++;
+					n_floats++;
 				}
-				nFloats.emplace_back(nValues);
-			} else {
-				nFloats.emplace_back(0);
 			}
-			if((mask_has_ints & (1<<type)) != 0) {
-				has_ints = true;
-				int index, nIndices = 0;
+			if(mask_has_ints & (1<<type)) {
+				int index;
 				if(type == e_el_f0) {
-					it++;
-					word = *it;
-					auto firstSlash = word.find("/"),
-						 lastSlash = word.rfind("/");
-					if(firstSlash != std::string::npos) {
-						if(firstSlash == lastSlash) {
-							type = e_el_f1;
-						} else if(firstSlash == lastSlash-1) {
-							type = e_el_f2;
-						} else {
-							type = e_el_f3;
-						}
-					} else {
-						type = e_el_f0;
-					}
 					auto s_it = std::begin(s_tk);
 					s_it++;
 					while(s_it != std::end(s_tk)) {
 						index = boost::lexical_cast<int>(*s_it++);
 						ints.push_back(index);
-						nIndices++;
+						n_ints++;
 					}
 				} else {
+					it++;
+					word = *it;
 					while(it != std::end(tk)) {
 						index = boost::lexical_cast<int>(*it++);
 						ints.push_back(index);
-						nIndices++;
+						n_ints++;
 					}
 				}
-				nInts.emplace_back(nIndices);
-			} else {
-				nInts.emplace_back(0);
 			}
-			if((mask_has_strings & (1<<type)) != 0) {
-				has_strings = true;
+			if(mask_has_strings & (1<<type)) {
 				std::ostringstream oss;
 				while(it != std::end(tk)) {
 					oss << *it++ << " ";
 				}
+				n_strings++;
 				strings.emplace_back(oss.str());
-				nStrings.emplace_back(1);
-			} else {
-				nStrings.emplace_back(0);
 			}
-			if(!has_bools && !has_floats && !has_ints && !has_strings) {
-				nBools.emplace_back(0);
-				nFloats.emplace_back(0);
-				nInts.emplace_back(0);
-				nStrings.emplace_back(0);
+			nBools.emplace_back(n_bools);
+			nFloats.emplace_back(n_floats);
+			nInts.emplace_back(n_ints);
+			nStrings.emplace_back(n_strings);
+			if(!n_bools && !n_floats && !n_ints && !n_strings) {
 				status = e_err_unknown;
 			} 
 			types.emplace_back(type);
-			if(type != prev) {
-				switch(prev) {
-					case e_el_v:
-						v_end.emplace_back(i);
-						break;
-					case e_el_vn:
-						vn_end.emplace_back(i);
-						break;
-					case e_el_vp:
-						vt_end.emplace_back(i);
-						break;
-					case e_el_f2:
-						f_end.emplace_back(j);
-						break;
-					default:
-						break;
-				}
-				switch(type) {
-					case e_el_v:
-						v_beg.emplace_back(i);
-						break;
-					case e_el_vn:
-						vn_beg.emplace_back(i);
-						break;
-					case e_el_vp:
-						vt_beg.emplace_back(i);
-						break;
-					case e_el_f2:
-						f_beg.emplace_back(j);
-						break;
-					default:
-						break;
-				}
-			}
-			if(mask_has_floats & (1<<type)) {
-				i++;
-			}
-			if(mask_has_ints & (1<<type)) {
-				j++;
-			}
-			prev = type;
 			break;
-		}
-		switch(prev) {
-			case e_el_v:
-				v_end.emplace_back(i);
-				break;
-			case e_el_vn:
-				vn_end.emplace_back(i);
-				break;
-			case e_el_vp:
-				vt_end.emplace_back(i);
-				break;
-			default:
-				break;
 		}
 		return status;
 	}
@@ -184,12 +117,53 @@ namespace Model {
 			return e_err_io;
 		}
 		e_el element;
-		e_status status = e_ok;
-
+		auto status = e_ok;
 		for(std::string line; std::getline(file, line);) {
-			auto st = obj.parse(line, " ");
+			auto type = obj.parse_type(line);
+			auto st = obj.parse(line, type);
 			if(status == e_ok) status = st;
-			// TODO Break or no?
+		}
+		auto prev = e_el_total;
+		int i = 0, nb = 0, nf = 0, ni = 0, ns = 0;
+		for(auto cur : obj.types) {
+			if(cur != prev) {
+				switch(cur) {
+					case e_el_v: obj.v_beg.emplace_back(nf); break;
+					case e_el_vn: obj.vn_beg.emplace_back(nf); break;
+					case e_el_vp: obj.vp_beg.emplace_back(nf); break;
+					case e_el_f0: obj.f0_beg.emplace_back(ni); break;
+					case e_el_f1: obj.f1_beg.emplace_back(ni); break;
+					case e_el_f2: obj.f2_beg.emplace_back(ni); break;
+					case e_el_f3: obj.f3_beg.emplace_back(ni); break;
+					default: break;
+				}
+				switch(prev) {
+					case e_el_v: obj.v_end.emplace_back(nf); break;
+					case e_el_vn: obj.vn_end.emplace_back(nf); break;
+					case e_el_vp: obj.vp_end.emplace_back(nf); break;
+					case e_el_f0: obj.f0_end.emplace_back(ni); break;
+					case e_el_f1: obj.f1_end.emplace_back(ni); break;
+					case e_el_f2: obj.f2_end.emplace_back(ni); break;
+					case e_el_f3: obj.f3_end.emplace_back(ni); break;
+					default: break;
+				}
+			}
+			nb += obj.nBools[i];
+			nf += obj.nFloats[i];
+			ni += obj.nInts[i];
+			ns += obj.nStrings[i];
+			prev = cur;
+			i++;
+		}
+		switch(prev) {
+			case e_el_v: obj.v_end.emplace_back(nf); break;
+			case e_el_vn: obj.vn_end.emplace_back(nf); break;
+			case e_el_vp: obj.vp_end.emplace_back(nf); break;
+			case e_el_f0: obj.f0_end.emplace_back(ni); break;
+			case e_el_f1: obj.f1_end.emplace_back(ni); break;
+			case e_el_f2: obj.f2_end.emplace_back(ni); break;
+			case e_el_f3: obj.f3_end.emplace_back(ni); break;
+			default: break;
 		}
 
 		file.close();
