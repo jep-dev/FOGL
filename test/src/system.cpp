8b1e66ecd459642169e54d21ba56c8dfe16b03c8
John Petersen <sobeita@gmail.com>
2016-09-01 15:47:51 -0400

    Converted Boost unit test to standard assertions and tests

--- test/src/system.cpp
+++ test/src/system.cpp
@@ -8,14 +8,17 @@ int main(int argc, const char **argv) {
 	assert(!Printer_Base::uni_special(0x79));
 	assert(Printer_Base::strlen("\e[38;5;190mA\e[0m")==1);
 	int width = 25;
+	double val = 1.234567901234;
 	std::cout << Printer_Base::align("Left-aligned", width,
-			Printer_Base::LEFT) << std::endl;
+			Printer_Base::LEFT, '_') << std::endl;
 	std::cout << Printer_Base::align("Centered", width,
-			Printer_Base::CENTER) << std::endl;
+			Printer_Base::CENTER, '_') << std::endl;
+	std::cout << Printer_Base::align(val, width,
+			Printer_Base::CENTER, '_') << std::endl;
+	std::cout << Printer_Base::align(val, width,
+			Printer_Base::CENTER, '_', 8) << std::endl;
 	std::cout << Printer_Base::align("Right-aligned", width,
-			Printer_Base::RIGHT) << std::endl;
-	std::cout << Printer_Base::align(1.2345, width,
-			Printer_Base::CENTER) << std::endl;
+			Printer_Base::RIGHT, '_') << std::endl;
 }
 
 /*BOOST_AUTO_TEST_CASE(uni_special) {
