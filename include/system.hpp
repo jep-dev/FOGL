c3567c8639babe8fe3444c3996c782db3085d08a
John Petersen <sobeita@gmail.com>
2016-09-01 22:06:57 -0400

    Removed the remainder of stringify

--- include/system.hpp
+++ include/system.hpp
@@ -5,21 +5,10 @@
 #include <vector>
 
 namespace System {
-	/// \copydoc readFile
-	bool readFile(const char *fname, std::vector<std::string> &lines);
 	std::string repeat(int w, char c = ' ');
 	
 	/// Splits a string along tokens
 	int split(std::string const &line, std::vector<std::string> words);
-
-	/*! String cast (lexical cast operator) template
- 	 * \tparam The type of the instance to convert to a string
- 	 */
-	template<class T> std::string stringify(const T &t);
-
-	/// \copydoc stringify
-	template<class T1, class T2, class... TN>
-	std::string stringify(const T1 &t1, const T2 &t2, const TN &... tn);
 }
 
 #include "system/printer.hpp"
