24d6886db58c42fcab07f1324768d2dc460d6bd8
John Petersen <sobeita@gmail.com>
2016-09-02 11:53:22 -0400

    Added tests

--- test/src/math.cpp
+++ test/src/math.cpp
@@ -18,6 +18,10 @@ const dual<float> E0{R0}, E{R0, 1},
 BOOST_AUTO_TEST_CASE(quaternions) {
 	BOOST_REQUIRE(i*j == k);
 	BOOST_REQUIRE(j*i == -k);
+	BOOST_REQUIRE(i*k == -j);
+	BOOST_REQUIRE(k*i == j);
+	BOOST_REQUIRE(j*k == i);
+	BOOST_REQUIRE(k*j == -i);
 	BOOST_REQUIRE(i*i == -R1);
 	BOOST_REQUIRE(j*j == -R1);
 	BOOST_REQUIRE(k*k == -R1);
@@ -27,4 +31,5 @@ BOOST_AUTO_TEST_CASE(dual_quaternions) {
 	BOOST_REQUIRE(E != 0);
 	BOOST_REQUIRE(E*E == 0);
 	BOOST_REQUIRE(k*E*i == k*i*E);
+	BOOST_REQUIRE(k*(E*i) == (k*E)*i);
 }
