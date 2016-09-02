535fff549ced5d1959c154a33669916eae066f09
John Petersen <sobeita@gmail.com>
2016-09-02 15:32:52 -0400

    Added sample uses of dual

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -4,14 +4,29 @@
 #include <iostream>
 #include <sstream>
 
+template<typename T>
+std::string pad(const T &t) {
+	using namespace System;
+	return Printer_Base::align(t, 14, Printer_Base::CENTER);
+}
+template<typename T>
+std::string paren(const T &t) {
+	return "(" + pad(t) + ")";
+}
+template<typename T>
+std::string transforms(const T &outer, const T &inner) {
+	return paren(outer) + "*" + paren(inner) + "*" + paren(~outer)
+		+ " = " + pad(outer(inner));
+}
+
 int main(int argc, const char **argv) {
 	using namespace System;
 	using namespace Math;
 
-	quat<int> r0 = {0,0,0,0},
+	quat<float> r0 = {0,0,0,0},
 		r1 = {1,0,0,0}, ri = {0,1,0,0},
 		rj = {0,0,1,0}, rk = {0,0,0,1};
-	dual<int> d10 = {r1, r0}, di0 = {ri, r0},
+	dual<float> d10 = {r1, r0}, di0 = {ri, r0},
 		dj0 = {rj, r0}, dk0 = {rk, r0},
 		d01 = {r0, r1}, d0i = {r0, ri},
 		d0j = {r0, rj}, d0k = {r0, rk},
@@ -19,7 +34,7 @@ int main(int argc, const char **argv) {
 
 	Printer<10> printer;
 	std::string rows[10], cols[8];
-	dual<int> cells[64];
+	dual<float> cells[64];
 	rows[0] = rows[9] = "";
 
 	int row = 0;
@@ -35,6 +50,10 @@ int main(int argc, const char **argv) {
 		row++;
 	}
 	printer.push(&rows[0], &rows[0]+10)
-		.push<dual<int>, 8, 8>(cells, &cols[0], &cols[8]).level();
+		.push<dual<float>, 8, 8>(cells, &cols[0], &cols[8]).level();
 	std::cout << printer << std::endl;
+
+	std::cout << transforms(d10+d0i, d10+d0j) << std::endl;
+	std::cout << transforms(dj0, d10+d0i) << std::endl;
+	std::cout << transforms(d10+d0j, d10+d0i) << std::endl;
 }
