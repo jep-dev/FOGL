db9f921f00ca9f1a125609164b79475b4c23cce6
John Petersen <sobeita@gmail.com>
2016-09-06 13:09:58 -0400

    Fixed one mesh bug; one to go (stray triangle fan)

--- src/model/mesh.cpp
+++ src/model/mesh.cpp
@@ -5,15 +5,15 @@ namespace Model {
 		(float s, float t, std::vector<float> &vertices)) {
 		for(int i = 0; i < w; i++) {
 			for(int j = 0; j < h; j++) {
-				auto index = j * w + i;
+				auto index = 3*(j * w + i), i2 = 3, j2 = w*3;
 				fn(float(i)/w, float(j)/h, vertices);
 				if(i < w-1 && j < h-1) {
 					faces.emplace_back(index);
-					faces.emplace_back(index+1);
-					faces.emplace_back(index+w+1);
+					faces.emplace_back(index+i2);
+					faces.emplace_back(index+i2+j2);
 					faces.emplace_back(index);
-					faces.emplace_back(index+w+1);
-					faces.emplace_back(index+w);
+					faces.emplace_back(index+i2+j2);
+					faces.emplace_back(index+j2);
 				}
 			}
 		}
