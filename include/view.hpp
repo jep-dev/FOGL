978ee4a5b4a7b83c2f082018ea72a041e7b2ff0f
John Petersen <sobeita@gmail.com>
2016-09-09 13:33:15 -0400

    Documentation fix

--- include/view.hpp
+++ include/view.hpp
@@ -38,8 +38,8 @@ namespace View {
 
 		/** Compiles and links the given shaders
  		 * @param alive Shared state; false signals shutdown
-		 * @param vert Path to a GLSL vertex shader
-		 * @param frag Path to a GLSL fragment shader
+		 * @param vert_fname Path to a GLSL vertex shader
+		 * @param frag_fname Path to a GLSL fragment shader
 		 */
 		bool setProg(std::atomic_bool &alive,
 				const char *vert_fname, const char *frag_fname);
