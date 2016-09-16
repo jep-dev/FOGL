2fc1161a4b49a6f50d6b496df5ba7a1d3a384109
John Petersen <sobeita@gmail.com>
2016-09-16 17:33:50 -0400

    Increased samples; vsync on new device does not appear to work

--- src/view.cpp
+++ src/view.cpp
@@ -144,7 +144,7 @@ namespace View {
 		}
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-		glfwWindowHint(GLFW_SAMPLES, 4);
+		glfwWindowHint(GLFW_SAMPLES, 16);
 		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 		
 		if(!(win = glfwCreateWindow(680, 680, "View", NULL, NULL))) {
