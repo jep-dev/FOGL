54b97d7331113e4e2db0dd520639af58d0aa2668
John Petersen <sobeita@gmail.com>
2016-09-16 17:32:55 -0400

    Changed signature of transpose

--- include/math/matrix.hpp
+++ include/math/matrix.hpp
@@ -3,12 +3,13 @@
 
 namespace Math {
 	template<int W, int H, typename R>
-	static void transpose(R (&src)[W*H], R (&dest)[H*W]) {
+	static auto transpose(R (&src)[W*H], R (&dest)[H*W]) -> decltype(dest) {
 		for(int row = 0; row < H; row++) {
 			for(int col = 0; col < W; col++) {
 				dest[row*W+col] = src[col*H+row];
 			}
 		}
+		return dest;
 	}
 }
 
