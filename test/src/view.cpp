6ff122eeca29bb2c48ed8e7d12862e4793c56e96
John Petersen <sobeita@gmail.com>
2016-09-19 13:59:12 -0400

    Gen/bound/buffered data buffers to prevent seg fault @redraw

--- test/src/view.cpp
+++ test/src/view.cpp
@@ -4,33 +4,21 @@
 #include "view.hpp"
 #include <iostream>
 
-#ifndef GLFW_TRUE
-#define GLFW_TRUE 1
-#endif
-#ifndef GLFW_FALSE
-#define GLFW_FALSE 0
-#endif
-
 int main(int argc, const char **argv) {
 	using namespace View;
-	if(glfwInit() == GLFW_FALSE) {
-		std::cout << "Failed to initialize GLFW "
-			<< glfwGetVersionString() << std::endl;
-		return 1;
+	std::atomic_bool alive(true);
+	view v(alive, "share/shade.vert", "share/shade.frag");
+	if(alive) {
+		glGenBuffers(1, &v.ids[view::e_id_vbuf]);
+		glBindBuffer(GL_ARRAY_BUFFER, v.ids[view::e_id_vbuf]);
+		glBufferData(GL_ARRAY_BUFFER, 0, nullptr, GL_STATIC_DRAW);
+		glGenBuffers(1, &v.ids[view::e_id_fbuf]);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, v.ids[view::e_id_fbuf]);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, 0, nullptr, GL_STATIC_DRAW);
+		v.init(alive);
 	} else {
-		std::atomic_bool alive(true);
-		view v(alive);
-		if(alive) {
-			v.setProg(alive, "share/shade.vert", "share/shade.frag");
-		} else {
-			std::cout << "Failed to construct view." << std::endl;
-		}
-		if(alive) v.init(alive);
-		else {
-			std::cout << "Failed to initialize view." << std::endl;
-			return 1;
-		}
-		if(alive) v.run(alive);
-		//glfwTerminate();
+		std::cout << "Failed to initialize view." << std::endl;
+		return 1;
 	}
+	if(alive) v.run(alive);
 }
