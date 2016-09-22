0f581ac75909319a9f2bfe5325074590113e3c6f
John Petersen <sobeita@gmail.com>
2016-09-22 09:06:30 -0400

    Temporarily switched back to trimeshes

--- src/main.cpp
+++ src/main.cpp
@@ -51,7 +51,7 @@ int main(int argc, const char *argv[]) {
 			<< obj_fname << '.' << std::endl;
 	} else {
 		int w = 20, h = 20;
-		/*model = trimesh_t(w, h,
+		model = trimesh_t(w, h,
 		[](float s, float t, std::vector<float> &vertices) {
 			vertices.emplace_back(s);
 			vertices.emplace_back((t+1)/2);
@@ -61,16 +61,16 @@ int main(int argc, const char *argv[]) {
 			vertices.emplace_back(-s);
 			vertices.emplace_back(-(t+1)/2);
 			vertices.emplace_back(1-s*s-pow((t+1)/2,2));
-		});*/
-		model = hexmesh_t(w, h,
+		});
+		/*model = hexmesh_t(w, h,
 		[](float s, float t, std::vector<float> &vertices) {
 			vertices.emplace_back(s);
 			vertices.emplace_back(t);
 			float n = 24, theta = s*M_PI*n, phi = t*M_PI/2*n,
 				ct = cos(theta), st = sin(theta),
 				cp = cos(phi), sp = sin(phi);
-			vertices.emplace_back((1-s*s-pow((t+1)/2,2))*(1+.1*ct*sp));
-		});
+			vertices.emplace_back((1-s*s-pow((t+1)/2,2))*(1+.05*ct*sp));
+		});*/
 		std::cout << "Initialized model with ad-hoc mesh." << std::endl;
 	}
 
