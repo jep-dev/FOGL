a98e8f75a3c56431a4d7b245bad8e3ed2f09713d
John Petersen <sobeita@gmail.com>
2016-09-23 09:02:19 -0400

    Tested node type

--- test/src/model.cpp
+++ test/src/model.cpp
@@ -22,7 +22,7 @@ int main(int argc, const char **argv) {
 	} else {
 		obj_fname = OBJ_PATH;
 	}
-	obj_t obj;
+	/*obj_t obj;
 	endl(std::cout << "File: " << obj_fname);
 	switch(obj_t::load(obj_fname, obj)) {
 		case obj_t::e_err_io:
@@ -40,7 +40,7 @@ int main(int argc, const char **argv) {
 			break;
 		default:
 			break;
-	}
+	}*/
 
 	int w = 10, h = 10;
 	/*mesh_t mesh(w, h, [](float s, float t, std::vector<float> &vertices) {
@@ -49,7 +49,8 @@ int main(int argc, const char **argv) {
 		vertices.emplace_back(sin(theta)*sin(phi));
 		vertices.emplace_back(cos(phi));
 	});*/
-	auto mesh = trimesh_t(w, h, [](float s, float t, std::vector<float> &vertices) {
+	auto mesh = trimesh_t(w, h, [](float s, float t,
+				std::vector<float> &vertices) {
 		vertices.emplace_back(s);
 		vertices.emplace_back(t);
 		vertices.emplace_back(0);
@@ -59,27 +60,38 @@ int main(int argc, const char **argv) {
 		vertices.emplace_back(0);
 	});
 
-	std::vector<node> nodes(h*w);
+	graph g(h*w);
 	for(int row = 0, index = 0; row < h; row++) {
 		float t = row/float(h-1);
 		for(int col = 0; col < w; col++, index++) {
 			float s = col/float(w-1);
 			float x[3]{s,t,0};
-			nodes[index] = x;
+			g.nodes[index] = x;
 			if(row > 0)
-				node::connect(&nodes[index], &nodes[index-w]);
+				node::connect(&g.nodes[index], &g.nodes[index-w]);
 			if(col > 0)
-				node::connect(&nodes[index], &nodes[index-1]);
+				node::connect(&g.nodes[index], &g.nodes[index-1]);
+			if(row > 0 && col > 0)
+				node::connect(&g.nodes[index], &g.nodes[index-w-1]);
 		}
 	}
-
-	//std::cout << mesh << std::endl;
-	/*std::ofstream file;
-	file.open("share/sphere.obj");
+	mesh_t g_mesh;
+	for(auto n : g.nodes) {
+		g_mesh.vertices.emplace_back(n.x);
+		g_mesh.vertices.emplace_back(n.y);
+		g_mesh.vertices.emplace_back(n.z);
+	}
+	for(auto f : g.faces())
+		g_mesh.faces.emplace_back(f);
+	//std::cout << g_mesh << std::endl;
+	std::ofstream file;
+	file.open("share/sheet.obj");
 	if(!file.is_open()) {
 		std::cout << "Could not open file." << std::endl;
 	} else {
-		file << mesh;
+		file << g_mesh;
 		file.close();
-	}*/
+	}
+
+	//std::cout << mesh << std::endl;
 }
