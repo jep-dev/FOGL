d912381e2064478a8d1f293e447187616ea7a970
John Petersen <sobeita@gmail.com>
2016-09-23 16:33:46 -0400

    Removed problematic triangulation

--- include/model.hpp
+++ include/model.hpp
@@ -7,7 +7,6 @@
 
 namespace Model {
 	struct model {
-		void fan(std::vector<int> face);
 		std::vector<int> faces;
 		std::vector<float> vertices, normals, vertex_normals;
 		std::vector<std::string> comments;
