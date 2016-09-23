aad40643e7b6b485ab6a6b30c9f9222a2184e3a2
John Petersen <sobeita@gmail.com>
2016-09-23 09:01:55 -0400

    Added id for comparisons

--- include/model/node.hpp
+++ include/model/node.hpp
@@ -7,15 +7,25 @@
 namespace Model {
 	struct node;
 	struct node {
+		static int ids;
+		int id = ids++;
 		float x, y, z;
 		std::set<const node*> adj;
-		bool connected(const node *n);
+		bool connected(const node *n) const;
 		static void connect(node *a, node *b);
 		static void disconnect(node *a, node *b);
+		static void insert(node *src, node *dest, node *middle);
+		bool operator==(const node& node) const;
 		node& operator=(const float (&) [3]);
 		node& operator+=(const node *n);
 		node& operator-=(const node *n);
 		virtual ~node(void);
+	};
+	struct graph {
+		std::vector<node> nodes;
+		std::vector<int> faces(void);
+		graph(int n);
+		virtual ~graph(void);
 	};
 }
 
--- src/model/node.cpp
+++ src/model/node.cpp
@@ -1,7 +1,8 @@
 #include "model/node.hpp"
 
 namespace Model {
-	bool node::connected(const node *n) {
+	int node::ids = 0;
+	bool node::connected(const node *n) const {
 		return std::find(std::begin(adj), std::end(adj), n) != std::end(adj);
 	}
 	void node::connect(node *a, node *b) {
@@ -11,6 +12,16 @@ namespace Model {
 	void node::disconnect(node *a, node *b) {
 		*a -= b;
 		*b -= a;
+	}
+	void node::insert(node *src, node *dest, node *middle) {
+		disconnect(src, dest);
+		connect(src, middle);
+		connect(dest, middle);
+		*middle = {(src->x+dest->x)/2, (src->y+dest->y)/2,
+			(src->z+dest->z)/2};
+	}
+	bool node::operator==(const node &n) const {
+		return id == n.id;
 	}
 	node& node::operator=(const float (&p) [3]) {
 		x = p[0];
@@ -27,4 +38,28 @@ namespace Model {
 		return *this;
 	}
 	node::~node(void) {}
+	std::vector<int> graph::faces(void) {
+		// TODO prevent duplicates including rotations?
+		std::vector<int> output;
+		//int i = 0;
+		for(const auto n : nodes) {
+			for(const auto a : n.adj) {
+				/*int j = std::find(std::begin(nodes), std::end(nodes), *a)
+					- std::begin(nodes);*/
+				for(const auto b : a -> adj) {
+					/*int k = std::find(std::begin(nodes), std::end(nodes), *b)
+						- std::begin(nodes);*/
+					if(a -> connected(b) && b -> id != n.id) {
+						output.emplace_back(n.id);
+						output.emplace_back(a->id);
+						output.emplace_back(b->id);
+					}
+				}
+			}
+			//i++;
+		}
+		return output;
+	}
+	graph::graph(int n): nodes(n) {}
+	graph::~graph(void) {}
 }
